b66011824f04a38cfb425e28f4cdfb76ad9a89e1
Danylo Lubenets <ext-danylo.lubenets@here.com>
2018-08-27 14:57:05 +0300

    Added solution for 'Two Sum'

--- src/solutions/two-sum.cpp
+++ src/solutions/two-sum.cpp
@@ -1,18 +1,42 @@
 #include "gtest/gtest.h"
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
 
 namespace
 {
-    struct Solution
-    {
-        bool
-        solution( ) const
-        {
-            return false;
-        }
-    };
-}
-
-TEST(TwoSum, generic)
+class Solution
 {
-    EXPECT_TRUE( Solution( ).solution( ) );
+public:
+    vector< int >
+    twoSum( vector< int >& nums, int target )
+    {
+        unordered_map< int, int > num2index;
+        for ( int i = 0; i < int( nums.size( ) ); ++i )
+        {
+            auto const val = nums[ i ];
+            auto const diff_val = target - val;
+
+            auto it = num2index.find( diff_val );
+            if ( it != num2index.end( ) )
+            {
+                return {it->second, i};
+            }
+
+            num2index[ nums[ i ] ] = i;
+        }
+
+        return {};
+    }
+};
+}  // namespace
+
+TEST( TwoSum, generic )
+{
+    vector< int > nums = {2, 7, 11, 15};
+    Solution sn;
+    const vector< int > expected = {0, 1};
+
+    EXPECT_EQ( expected, sn.twoSum( nums, 9 ) );
 }
