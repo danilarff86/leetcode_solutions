0ca6e89429508a01c1e42f22860bbcfd2ac37e76
Danylo Lubenets <ext-danylo.lubenets@here.com>
2018-08-27 15:11:10 +0300

    Added solution for 'Two Sum II'

---
+++ src/solutions/two-sum-II.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+namespace
+{
+class Solution
+{
+public:
+    vector< int >
+    twoSum( vector< int >& numbers, int target )
+    {
+        int i = 0;
+        int j = numbers.size( ) - 1;
+        while ( i < j )
+        {
+            auto sum = numbers[ i ] + numbers[ j ];
+            if ( sum < target )
+            {
+                ++i;
+            }
+            else if ( sum > target )
+            {
+                --j;
+            }
+            else
+            {
+                return {i + 1, j + 1};
+            }
+        }
+        return {};
+    }
+};
+}  // namespace
+
+TEST( TwoSum_II, generic )
+{
+    vector< int > nums = {2, 7, 11, 15};
+    Solution sn;
+    const vector< int > expected = {1, 2};
+
+    EXPECT_EQ( expected, sn.twoSum( nums, 9 ) );
+}
