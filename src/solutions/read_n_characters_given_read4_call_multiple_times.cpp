5f9b4e45bc39b78ecac46cf6e20317b533a6ff58
Danylo Lubenets <ext-danylo.lubenets@here.com>
2018-12-06 18:33:42 +0200

    Added solution for 'Read N Characters Given Read4 – Call multiple times'

---
+++ src/solutions/read_n_characters_given_read4_call_multiple_times.cpp
@@ -0,0 +1,98 @@
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <vector>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+namespace
+{
+static string s_buf;
+static size_t s_offset;
+
+void
+set_buf( const string& str )
+{
+    s_buf = str;
+    s_offset = 0;
+}
+
+int
+read4( char* buf )
+{
+    const auto initial_offset = s_offset;
+    const auto end_offset = min( s_buf.size( ), s_offset + 4 );
+    while ( s_offset < end_offset )
+    {
+        *buf++ = s_buf[ s_offset++ ];
+    }
+
+    return s_offset - initial_offset;
+}
+
+class Solution
+{
+public:
+    Solution( )
+        : last_chunk_size( 0 )
+        , last_chunk_offset( 0 )
+    {
+    }
+
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int
+    read( char* buf, int n )
+    {
+        int read_chars = 0;
+
+        auto end_chunk_offset = min( size_t( n + last_chunk_offset ), last_chunk_size );
+        while ( last_chunk_offset < end_chunk_offset )
+        {
+            *buf++ = last_chunk_buf[ last_chunk_offset++ ];
+            ++read_chars;
+        }
+
+        int last_chunk = 4;
+        while ( read_chars < n && last_chunk == 4 )
+        {
+            last_chunk = read4( last_chunk_buf );
+            last_chunk_size = last_chunk;
+            last_chunk_offset = 0;
+
+            auto end_chunk_offset = min( size_t( n - read_chars ), last_chunk_size );
+            while ( last_chunk_offset < end_chunk_offset )
+            {
+                *buf++ = last_chunk_buf[ last_chunk_offset++ ];
+                ++read_chars;
+            }
+        }
+
+        return read_chars;
+    }
+private:
+    size_t last_chunk_size;
+    size_t last_chunk_offset;
+    char last_chunk_buf[4];
+};
+}  // namespace
+
+TEST( ReadNCharactersGivenRead4CallMultipleTimes, generic )
+{
+    char buf[ 1024 ];
+
+    auto sln = make_unique< Solution >( );
+    set_buf( "abc" );
+    EXPECT_EQ( "a", string( buf, sln->read( buf, 1 ) ) );
+    EXPECT_EQ( "bc", string( buf, sln->read( buf, 2 ) ) );
+    EXPECT_EQ( "", string( buf, sln->read( buf, 1 ) ) );
+
+    sln = make_unique< Solution >( );
+    set_buf( "abc" );
+    EXPECT_EQ( "abc", string( buf, sln->read( buf, 4 ) ) );
+    EXPECT_EQ( "", string( buf, sln->read( buf, 1 ) ) );
+}
