d9442ba5ab69be2dc0bd785781696e51d8124a58
Danylo Lubenets <ext-danylo.lubenets@here.com>
2018-12-08 12:41:47 +0200

    Added solution for 'Copy List with Random Pointer'

---
+++ src/solutions/copy_list_with_random_pointer.cpp
@@ -0,0 +1,50 @@
+#include "gtest/gtest.h"
+#include "linked_list.h"
+
+#include <unordered_map>
+
+using namespace std;
+using namespace linked_list;
+
+namespace
+{
+class Solution
+{
+public:
+    RandomListNode*
+    copyRandomList( RandomListNode* head )
+    {
+        unordered_map< RandomListNode*, RandomListNode* > mapping;
+        RandomListNode res( 0 );
+        auto res_node = &res;
+        auto head_node = head;
+        while ( head_node != nullptr )
+        {
+            res_node->next = new RandomListNode( head_node->label );
+            mapping[ head_node ] = res_node->next;
+            head_node = head_node->next;
+            res_node = res_node->next;
+        }
+
+        res_node = &res;
+        head_node = head;
+        while ( head_node != nullptr )
+        {
+            res_node->next->random = mapping[ head_node->random ];
+            head_node = head_node->next;
+            res_node = res_node->next;
+        }
+
+        return res.next;
+    }
+};
+}  // namespace
+
+TEST( CopyListWithRandomPointer, generic )
+{
+    Solution sn;
+    {
+        RandomListNodeInfoVec v{{1, -1}, {2, 0}};
+        EXPECT_EQ( v, random_list_to_vector( sn.copyRandomList( random_list_from_vector( v ) ) ) );
+    }
+}
