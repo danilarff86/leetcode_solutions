3a4a131d3e3cb044a2372d198cef55247f702568
Danylo Lubenets <ext-danylo.lubenets@here.com>
2018-12-11 17:58:03 +0200

    Added solution for 'Balanced Binary Tree'

---
+++ src/solutions/balanced_binary_tree.cpp
@@ -0,0 +1,52 @@
+#include "binary_tree.h"
+#include "gtest/gtest.h"
+
+#include <algorithm>
+
+using namespace std;
+using namespace binary_tree;
+
+namespace
+{
+class Solution
+{
+public:
+    bool
+    isBalanced( const TreeNode* root )
+    {
+        return max_depth( root ) != -1;
+    }
+
+private:
+    int
+    max_depth( const TreeNode* node )
+    {
+        if ( node == nullptr )
+        {
+            return 0;
+        }
+
+        const int L = max_depth( node->left );
+        if ( L == -1 )
+        {
+            return -1;
+        }
+
+        const int R = max_depth( node->right );
+        if ( R == -1 )
+        {
+            return -1;
+        }
+
+        return ( std::abs( L - R ) <= 1 ) ? ( std::max( L, R ) + 1 ) : -1;
+    }
+};
+
+}  // namespace
+
+TEST( BalancedBinaryTree, generic )
+{
+    Solution sn;
+    EXPECT_TRUE( sn.isBalanced( bt_from_vec( {3, 9, 20, -1, -1, 15, 7} ) ) );
+    EXPECT_FALSE( sn.isBalanced( bt_from_vec( {1, 2, 2, 3, 3, -1, -1, 4, 4} ) ) );
+}
