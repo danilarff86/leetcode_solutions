7f35687ea3a65ac16c8f15d63c9ef632486dc509
Danylo Lubenets <ext-danylo.lubenets@here.com>
2019-01-03 19:06:22 +0200

    Added solution for 'Find Minimum in Rotated Sorted Array'

---
+++ solutions/find_minimum_in_rotated_sorted_array.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+
+using namespace std;
+
+namespace
+{
+class Solution
+{
+public:
+    int
+    findMin( const vector< int >& nums )
+    {
+        int i = 0;
+        int j = nums.size( ) - 1;
+        while ( i < j && nums[ i ] > nums[ j ] )
+        {
+            const int m = ( i + j ) / 2;
+            if ( nums[ i ] < nums[ m ] )
+            {
+                i = m + 1;
+            }
+            else
+            {
+                j = m;
+            }
+        }
+        return nums[ i ];
+    }
+};
+}  // namespace
+
+TEST( FindMinimumInRotatedSortedArray, generic )
+{
+    Solution sn;
+    EXPECT_EQ( 1, sn.findMin( {3, 4, 5, 1, 2} ) );
+    EXPECT_EQ( 0, sn.findMin( {4, 5, 6, 7, 0, 1, 2} ) );
+}
