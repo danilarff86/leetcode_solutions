f94757d166fc6d045167e7cea46f6a78a60117b2
Danylo Lubenets <danilarff86@gmail.com>
2019-12-08 21:24:15 +0200

    Added dp solution for 'Delete Operation for Two Strings'

--- solutions/delete-operation-for-two-strings.cpp
+++ solutions/delete-operation-for-two-strings.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 namespace
 {
+namespace recurse_with_memoization
+{
 class Solution
 {
 public:
@@ -48,10 +50,43 @@ public:
 private:
     vector< vector< int > > m_cache;
 };
+}  // namespace recurse_with_memoization
+
+namespace dp
+{
+class Solution
+{
+public:
+    int
+    minDistance( const string& word1, const string& word2 )
+    {
+        vector< vector< int > > dp( word1.size( ) + 1, vector< int >( word2.size( ) + 1 ) );
+        for ( size_t i = 0; i <= word1.size( ); i++ )
+        {
+            for ( size_t j = 0; j <= word2.size( ); j++ )
+            {
+                if ( i == 0 || j == 0 )
+                {
+                    continue;
+                }
+                if ( word1[ i - 1 ] == word2[ j - 1 ] )
+                {
+                    dp[ i ][ j ] = 1 + dp[ i - 1 ][ j - 1 ];
+                }
+                else
+                {
+                    dp[ i ][ j ] = max( dp[ i - 1 ][ j ], dp[ i ][ j - 1 ] );
+                }
+            }
+        }
+        return word1.size( ) + word2.size( ) - 2 * dp[ word1.size( ) ][ word2.size( ) ];
+    }
+};
+}  // namespace dp
 }  // namespace
 
 TEST( DeleteOperationForTwoStrings, generic )
 {
-    Solution sn;
+    dp::Solution sn;
     EXPECT_EQ( 2, sn.minDistance( "sea", "eat" ) );
 }
