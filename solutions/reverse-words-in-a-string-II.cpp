b69b3558a93d9af4ca500fd31e37749f52ff8529
Danylo Lubenets <danilarff86@gmail.com>
2020-01-28 16:26:59 +0200

    Updated reverse-words solutions

--- solutions/reverse-words-in-a-string-II.cpp
+++ solutions/reverse-words-in-a-string-II.cpp
@@ -1,10 +1,11 @@
 #include <algorithm>
 #include <vector>
+
 #include "gtest/gtest.h"
 
 using namespace std;
 
-namespace
+namespace old_variant
 {
 class Solution
 {
@@ -25,14 +26,52 @@ public:
         }
     }
 };
-}  // namespace
+}  // namespace old_variant
+
+namespace new_variant
+{
+class Solution
+{
+public:
+    void
+    reverseWords( vector< char >& str )
+    {
+        std::reverse( str.begin( ), str.end( ) );
+        auto cit = str.begin( );
+        while ( cit != str.end( ) )
+        {
+            const auto word_begin = cit;
+            while ( cit != str.end( ) && !isspace( *cit ) )
+            {
+                ++cit;
+            }
+            std::reverse( word_begin, cit );
+            if ( isspace( *cit ) )
+            {
+                ++cit;
+            }
+        }
+    }
+};
+}  // namespace new_variant
 
 TEST( ReverseWordsInAStringII, generic )
 {
-    Solution sn;
+    {
+        old_variant::Solution sn;
 
-    string s1 = "the sky is blue";
-    vector< char > v1( s1.begin( ), s1.end( ) );
-    sn.reverseWords( v1 );
-    EXPECT_EQ( "blue is sky the", string( v1.data( ), v1.size( ) ) );
+        string s1 = "the sky is blue";
+        vector< char > v1( s1.begin( ), s1.end( ) );
+        sn.reverseWords( v1 );
+        EXPECT_EQ( "blue is sky the", string( v1.data( ), v1.size( ) ) );
+    }
+
+    {
+        new_variant::Solution sn;
+
+        string s1 = "the sky is blue";
+        vector< char > v1( s1.begin( ), s1.end( ) );
+        sn.reverseWords( v1 );
+        EXPECT_EQ( "blue is sky the", string( v1.data( ), v1.size( ) ) );
+    }
 }
--- solutions/reverse-words-in-a-string.cpp
+++ solutions/reverse-words-in-a-string.cpp
@@ -2,11 +2,12 @@
 #include <iterator>
 #include <string>
 #include <vector>
+
 #include "gtest/gtest.h"
 
 using namespace std;
 
-namespace
+namespace old_variant
 {
 class Solution
 {
@@ -48,25 +49,80 @@ public:
         s.assign( s2.begin( ), s2.end( ) );
     }
 };
-}  // namespace
+}  // namespace old_variant
+
+namespace new_variant
+{
+class Solution
+{
+public:
+    void
+    reverseWords( string& s )
+    {
+        string res;
+        res.reserve( s.length( ) );
+
+        int j = s.length( );
+        for ( int i = s.length( ) - 1; i >= 0; i-- )
+        {
+            if ( isspace( s[ i ] ) )
+            {
+                j = i;
+            }
+            else if ( i == 0 || isspace( s[ i - 1 ] ) )
+            {
+                if ( !res.empty( ) )
+                {
+                    res.push_back( ' ' );
+                }
+                res.append( s.substr( i, j - i ) );
+            }
+        }
+
+        s = move( res );
+    }
+};
+}  // namespace new_variant
 
 TEST( ReverseWordsInAString, generic )
 {
-    Solution sn;
+    {
+        old_variant::Solution sn;
 
-    std::string s1 = "the sky is blue";
-    sn.reverseWords( s1 );
-    EXPECT_EQ( "blue is sky the", s1 );
+        std::string s1 = "the sky is blue";
+        sn.reverseWords( s1 );
+        EXPECT_EQ( "blue is sky the", s1 );
 
-    std::string s2 = "  the sky is blue  ";
-    sn.reverseWords( s2 );
-    EXPECT_EQ( "blue is sky the", s2 );
+        std::string s2 = "  the sky is blue  ";
+        sn.reverseWords( s2 );
+        EXPECT_EQ( "blue is sky the", s2 );
 
-    std::string s3 = " 1";
-    sn.reverseWords( s3 );
-    EXPECT_EQ( "1", s3 );
+        std::string s3 = " 1";
+        sn.reverseWords( s3 );
+        EXPECT_EQ( "1", s3 );
 
-    std::string s4 = "hi!";
-    sn.reverseWords( s4 );
-    EXPECT_EQ( "hi!", s4 );
+        std::string s4 = "hi!";
+        sn.reverseWords( s4 );
+        EXPECT_EQ( "hi!", s4 );
+    }
+
+    {
+        new_variant::Solution sn;
+
+        std::string s1 = "the sky is blue";
+        sn.reverseWords( s1 );
+        EXPECT_EQ( "blue is sky the", s1 );
+
+        std::string s2 = "  the sky is blue  ";
+        sn.reverseWords( s2 );
+        EXPECT_EQ( "blue is sky the", s2 );
+
+        std::string s3 = " 1";
+        sn.reverseWords( s3 );
+        EXPECT_EQ( "1", s3 );
+
+        std::string s4 = "hi!";
+        sn.reverseWords( s4 );
+        EXPECT_EQ( "hi!", s4 );
+    }
 }
