5335ea16482fb5316a5694d0b25daf440679207f
Danylo Lubenets <danilarff86@gmail.com>
2020-01-30 12:30:07 +0200

    Merged sort for arrays

--- solutions/merge-two-sorted-lists.cpp
+++ solutions/merge-two-sorted-lists.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace linked_list;
 
-namespace
+namespace merge_lists
 {
 class Solution
 {
@@ -42,14 +42,56 @@ public:
         return res.next;
     }
 };
-}  // namespace
+}  // namespace merge_lists
+
+namespace merge_arrays
+{
+class Solution
+{
+public:
+    vector< int >
+    mergeArrays( const vector< int >& v1, const vector< int >& v2 )
+    {
+        vector< int > res( v1.size( ) + v2.size( ) );
+        auto it1 = v1.begin( );
+        auto it2 = v2.begin( );
+        auto itRes = res.begin( );
+
+        while ( it1 != v1.end( ) && it2 != v2.end( ) )
+        {
+            if ( *it1 < *it2 )
+            {
+                *itRes++ = *it1++;
+            }
+            else
+            {
+                *itRes++ = *it2++;
+            }
+        }
+
+        auto itBegin = it1 != v1.end( ) ? it1 : it2;
+        auto itEnd = it1 != v1.end( ) ? v1.end( ) : v2.end( );
+
+        std::copy( itBegin, itEnd, itRes );
+
+        return res;
+    }
+};
+}  // namespace merge_arrays
 
 TEST( MergeTwoSortedLists, generic )
 {
-    Solution sn;
-    EXPECT_EQ( vector< int >( {1, 1, 2, 3, 4, 4} ),
-               list_to_vector( sn.mergeTwoLists( list_from_vector( {1, 2, 4} ),
-                                                 list_from_vector( {1, 3, 4} ) ) ) );
-    EXPECT_EQ( vector< int >( ), list_to_vector( sn.mergeTwoLists( list_from_vector( {} ),
-                                                                   list_from_vector( {} ) ) ) );
+    {
+        merge_lists::Solution sn;
+        EXPECT_EQ( vector< int >( {1, 1, 2, 3, 4, 4} ),
+                   list_to_vector( sn.mergeTwoLists( list_from_vector( {1, 2, 4} ),
+                                                     list_from_vector( {1, 3, 4} ) ) ) );
+        EXPECT_EQ( vector< int >( ), list_to_vector( sn.mergeTwoLists( list_from_vector( {} ),
+                                                                       list_from_vector( {} ) ) ) );
+    }
+    {
+        merge_arrays::Solution sn;
+        EXPECT_EQ( vector< int >( {1, 1, 2, 3, 4, 4} ), sn.mergeArrays( {1, 2, 4}, {1, 3, 4} ) );
+        EXPECT_EQ( vector< int >( ), sn.mergeArrays( {}, {} ) );
+    }
 }
