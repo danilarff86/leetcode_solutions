78ae9ce7a7269d6a7e2a7db0c2e7533f89e7240d
Danylo Lubenets <danilarff86@gmail.com>
2020-02-02 13:35:21 +0200

    Added solution for 'K Closest Points to Origin'

---
+++ solutions/k-closest-points-to-origin.cpp
@@ -0,0 +1,63 @@
+#include <queue>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+using namespace std;
+
+namespace
+{
+class Solution
+{
+public:
+    vector< vector< int > >
+    kClosest( vector< vector< int > >& points, int K )
+    {
+        const auto sz = points.size( );
+        vector< int > squared_euclidean_distances( sz );
+        for ( size_t i = 0; i < sz; ++i )
+        {
+            const auto q1 = points[ i ][ 0 ];
+            const auto q2 = points[ i ][ 1 ];
+            squared_euclidean_distances[ i ] = q1 * q1 + q2 * q2;
+        }
+
+        auto comp = [&squared_euclidean_distances]( size_t lhs_index, size_t rhs_index ) {
+            return squared_euclidean_distances[ lhs_index ]
+                   > squared_euclidean_distances[ rhs_index ];
+        };
+
+        priority_queue< size_t, vector< size_t >, decltype( comp ) > indices_of_closest_points(
+            comp );
+
+        for ( size_t i = 0; i < sz; ++i )
+        {
+            indices_of_closest_points.push( i );
+        }
+
+        vector< vector< int > > res( K );
+
+        for ( int i = 0; i < K; ++i )
+        {
+            res[ i ] = std::move( points[ indices_of_closest_points.top( ) ] );
+            indices_of_closest_points.pop( );
+        }
+
+        return res;
+    }
+};
+}  // namespace
+
+TEST( KClosestPointsToOrigin, generic )
+{
+    Solution sn;
+    {
+        vector< vector< int > > points( {{1, 3}, {-2, 2}} );
+        EXPECT_EQ( vector< vector< int > >( {{-2, 2}} ), sn.kClosest( points, 1 ) );
+    }
+
+    {
+        vector< vector< int > > points( {{3, 3}, {5, -1}, {-2, 4}} );
+        EXPECT_EQ( vector< vector< int > >( {{3, 3}, {-2, 4}} ), sn.kClosest( points, 2 ) );
+    }
+}
