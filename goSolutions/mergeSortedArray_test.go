582e363d0f6e129c0a42067487cd7f8a73e68b01
Danylo Lubenets <danilarff86@gmail.com>
2020-07-28 17:01:34 +0300

    Added solution for 'Merge Sorted Array'

---
+++ goSolutions/mergeSortedArray_test.go
@@ -0,0 +1,61 @@
+package goSolutions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for i := m - 1; i >= 0; i-- {
+		nums1[i+n] = nums1[i]
+	}
+
+	i1 := n
+	i2 := 0
+	i := 0
+	for i1 < (m+n) && i2 < n {
+		if nums1[i1] < nums2[i2] {
+			nums1[i] = nums1[i1]
+			i1++
+		} else {
+			nums1[i] = nums2[i2]
+			i2++
+		}
+		i++
+	}
+
+	for ; i1 < (m + n); i1++ {
+		nums1[i] = nums1[i1]
+		i++
+	}
+
+	for ; i2 < n; i2++ {
+		nums1[i] = nums2[i2]
+		i++
+	}
+}
+
+func TestMergeSortedArrayBasic(t *testing.T) {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, len(nums1)-len(nums2), nums2, len(nums2))
+
+	require.Equal(t, []int{1, 2, 2, 3, 5, 6}, nums1)
+}
+
+func TestMergeSortedArraySecondArrayIsEmpty(t *testing.T) {
+	nums1 := []int{1}
+	nums2 := []int{}
+	merge(nums1, len(nums1)-len(nums2), nums2, len(nums2))
+
+	require.Equal(t, []int{1}, nums1)
+}
+
+func TestMergeSortedArrayCase12(t *testing.T) {
+	nums1 := []int{4, 0, 0, 0, 0, 0}
+	nums2 := []int{1, 2, 3, 5, 6}
+	merge(nums1, len(nums1)-len(nums2), nums2, len(nums2))
+
+	require.Equal(t, []int{1, 2, 3, 4, 5, 6}, nums1)
+}
