e015569781e4ed333da7b2c7b3f5af87b281b1ba
Danylo Lubenets <danilarff86@gmail.com>
2021-05-02 22:03:48 +0300

    Added go solution for 'Triangle'

---
+++ gosolutions/triangle_test.go
@@ -0,0 +1,39 @@
+package gosolutions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func min(v1, v2 int) int {
+	if v1 < v2 {
+		return v1
+	}
+	return v2
+}
+
+func minimumTotal(triangle [][]int) int {
+	tLen := len(triangle)
+	dp := make([][]int, tLen)
+	dp[0] = make([]int, 1)
+	dp[0][0] = triangle[0][0]
+	for iRow := 1; iRow < tLen; iRow++ {
+		dp[iRow] = make([]int, iRow+1)
+		dp[iRow][0] = dp[iRow-1][0] + triangle[iRow][0]
+		dp[iRow][iRow] = dp[iRow-1][iRow-1] + triangle[iRow][iRow]
+		for iCol := 1; iCol < iRow; iCol++ {
+			dp[iRow][iCol] = triangle[iRow][iCol] + min(dp[iRow-1][iCol-1], dp[iRow-1][iCol])
+		}
+	}
+	res := 0xFFFFFFFF
+	for _, v := range dp[tLen-1] {
+		res = min(res, v)
+	}
+	return res
+}
+
+func TestTriangle(t *testing.T) {
+	assert.Equal(t, 11, minimumTotal([][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}))
+	assert.Equal(t, -10, minimumTotal([][]int{{-10}}))
+}
