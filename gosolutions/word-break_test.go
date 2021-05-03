feeb9d639b57f7811865a2b3f8c855480d717e7f
Danylo Lubenets <danilarff86@gmail.com>
2021-05-03 17:57:44 +0300

    Added go solution for 'Word Break'

---
+++ gosolutions/word-break_test.go
@@ -0,0 +1,34 @@
+package gosolutions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func wordBreak(s string, wordDict []string) bool {
+	dict := make(map[string]struct{})
+	for _, w := range wordDict {
+		dict[w] = struct{}{}
+	}
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j <= i; j++ {
+			if dp[j] {
+				_, ok := dict[s[j:i]]
+				if ok {
+					dp[i] = true
+					break
+				}
+			}
+		}
+	}
+	return dp[len(s)]
+}
+
+func TestWordBreak(t *testing.T) {
+	assert.True(t, wordBreak("leetcode", []string{"leet", "code"}))
+	assert.True(t, wordBreak("applepenapple", []string{"apple", "pen"}))
+	assert.False(t, wordBreak("catsandog", []string{"cats", "dog", "sand", "and", "cat"}))
+}
