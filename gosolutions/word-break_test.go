8e70194436f7fe6226135173c24d7efe297ddbae
Danylo Lubenets <danilarff86@gmail.com>
2022-08-21 11:40:20 +0300

    Added another go solution for 'Word Break'

--- gosolutions/word-break_test.go
+++ gosolutions/word-break_test.go
@@ -27,8 +27,33 @@ func wordBreak(s string, wordDict []string) bool {
 	return dp[len(s)]
 }
 
+func wordBreakDP2(s string, wordDict []string) bool {
+	dict := make(map[string]struct{})
+	for _, w := range wordDict {
+		dict[w] = struct{}{}
+	}
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if dp[j] {
+				_, ok := dict[s[j:i]]
+				if ok {
+					dp[i] = true
+					break
+				}
+			}
+		}
+	}
+	return dp[len(s)]
+}
+
 func TestWordBreak(t *testing.T) {
 	assert.True(t, wordBreak("leetcode", []string{"leet", "code"}))
 	assert.True(t, wordBreak("applepenapple", []string{"apple", "pen"}))
 	assert.False(t, wordBreak("catsandog", []string{"cats", "dog", "sand", "and", "cat"}))
+
+	assert.True(t, wordBreakDP2("leetcode", []string{"leet", "code"}))
+	assert.True(t, wordBreakDP2("applepenapple", []string{"apple", "pen"}))
+	assert.False(t, wordBreakDP2("catsandog", []string{"cats", "dog", "sand", "and", "cat"}))
 }
