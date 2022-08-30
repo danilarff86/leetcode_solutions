e6ec3d9293b18ac6537a8074826d91ddb77879ae
Danylo Lubenets <danilarff86@gmail.com>
2022-08-30 16:38:43 +0300

    Another go solution for 'Best Time to Buy and Sell Stock'

--- gosolutions/best-time-to-buy-and-sell-stock_test.go
+++ gosolutions/best-time-to-buy-and-sell-stock_test.go
@@ -20,7 +20,24 @@ func maxProfit(prices []int) int {
 	return maxProfit
 }
 
+func maxProfitNew(prices []int) int {
+	profit := 0
+	minVal := prices[0]
+	for i := 1; i < len(prices); i++ {
+		currentProfit := prices[i] - minVal
+		if currentProfit > profit {
+			profit = currentProfit
+		} else if prices[i] < minVal {
+			minVal = prices[i]
+		}
+	}
+	return profit
+}
+
 func TestBestTimeToBuyAndSellStock(t *testing.T) {
 	assert.Equal(t, 5, maxProfit([]int{7, 1, 5, 3, 6, 4}))
 	assert.Equal(t, 0, maxProfit([]int{7, 6, 4, 3, 1}))
+
+	assert.Equal(t, 5, maxProfitNew([]int{7, 1, 5, 3, 6, 4}))
+	assert.Equal(t, 0, maxProfitNew([]int{7, 6, 4, 3, 1}))
 }
